2f5abe5e6e8daccd872a62784d5c5c7d594fa964
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-05-16 21:33:36 +0200

    layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-export default function App(){
-
-    return (
-        <h1>Portfolio</h1>
-    )
+export default function App() {
+  return (
+    <div className="h-screen flex max-w-3xl m-auto">
+      <div className="my-5 shadow-2xl rounded flex-col w-full border-2 border-slate-100 p-3 flex" style={{ borderRadius: "30px" }}>
+        <div className="bg-red-500 w-full h-64"></div>
+        <div className="h-full w-full bg-black"></div>
+        <h1 className="text-center">Designed with love by Ondřej Hájek 💘</h1>
+      </div>
+    </div>
+  );
 }
