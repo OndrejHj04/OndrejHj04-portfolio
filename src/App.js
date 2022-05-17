86d18e40f03d5d02619ccecb63aef9ccd4324dc2
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-05-17 06:36:12 +0200

    Mobile height II

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import {useState } from "react";
+import { useState } from "react";
 
 export default function App() {
+  const [height, setHeight] = useState(window.innerHeight);
 
-    const [height, setHeight] = useState()
+  window.addEventListener("resize", () => setHeight(window.innerHeight));
 
-    window.addEventListener("resize", ()=>setHeight(window.innerHeight))
-    console.log(height)
   return (
-    <div className="flex max-w-3xl m-auto" style={{height: height, minHeight: "450px"}}>
+    <div className="flex max-w-3xl m-auto" style={{ height: height, minHeight: "450px" }}>
       <div className="my-5 shadow-2xl rounded flex-col w-full border-2 border-slate-100 p-3 flex" style={{ borderRadius: "30px" }}>
         <div className="bg-red-500 w-full h-64"></div>
         <div className="h-full w-full bg-black"></div>
