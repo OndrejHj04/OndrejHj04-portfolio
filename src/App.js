5a47685b8736ae1fc717830f76a61ee9ea18170c
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-05-17 07:52:18 +0200

    eslint w

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ export default function App() {
     <div className="flex max-w-3xl m-auto" style={{ height: height, minHeight: "450px" }}>
       <div className="m-3 shadow-2xl rounded flex-col w-full border-2 border-slate-100 p-3 flex" style={{ borderRadius: "30px" }}>
         {repos && getRepos()[counter]}
-        <h1 className="text-center">Designed with love 💘 <br />by <a href="https://github.com/OndrejHj04" target="_blank" className="text-sky-500 font-semibold">Ondřej Hájek</a></h1>
+        <h1 className="text-center">Designed with love 💘 <br />by <a href="https://github.com/OndrejHj04" target="_blank" rel="noreferrer" className="text-sky-500 font-semibold">Ondřej Hájek</a></h1>
       </div>
     </div>
   );
